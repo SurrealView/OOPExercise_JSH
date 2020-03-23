fe2b9492a859f26df9b21b8d5c503bcadbba9061
Quartermoon <Quartermoon@DESKTOP-94OSCIF>
2020-03-23 20:19:50 +0900

    Add MemberManager Member value and default creator

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -1,6 +1,18 @@
 package member.controller;
 
+import java.util.Scanner;
+
+import member.model.vo.Member;
+
 public class MemberManager {
+
+	private static Member[] m = new Member[10];
+	private static int ctn = 0; // 회원 수 체크용 변수
+	private Scanner sc = new Scanner(System.in); // 키보드로 정보 입력 받는 용
+
+	public MemberManager() {
+
+	}
 
 	public void insertMember() {
 		// TODO Auto-generated method stub
@@ -29,42 +41,42 @@ public class MemberManager {
 
 	public void searchId() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void searchName() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void searchEmail() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void updatePwd() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void updateName() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void updateEmail() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void deleteOne() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void deleteAll() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }

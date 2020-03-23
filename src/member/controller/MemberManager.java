aacab034fc046f3b860c894257cfbeda3cc6de9d
Quartermoon <Quartermoon@DESKTOP-94OSCIF>
2020-03-23 20:36:13 +0900

    Completed searchMemberMenu() and printOne() in MemberManager

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -26,25 +26,40 @@ public class MemberManager {
 		System.out.print("성별 입력 : ");
 		char gender = sc.next().charAt(0);
 		System.out.print("이메일 입력 :");
-		String email = sc.next();		
+		String email = sc.next();
 		// 키보드로 아이디, 패스워드, 이름, 나이, 성별, 이메일 입력
-		
-		m[MemberManager.ctn] = new Member(id,pw,name,age,gender,email);
+
+		m[MemberManager.ctn] = new Member(id, pw, name, age, gender, email);
 		// 입력 받은 정보를 Member클래스의 매개변수 있는 생성자를 사용해 객체 생성 후 배열에 저장
-		
+
 		MemberManager.ctn++;
 		// 인덱스와 회원수 증가시킴
-		
+
 		System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다.");
 		// "입력이 완료되었습니다. 메인 메뉴로 돌아갑니다." 출력 후 메인메뉴로 돌아감
 		return;
 	}
 
 	public void searchMemberMenu() {
-		// TODO Auto-generated method stub
+		System.out.print("검색할 아이디를 입력하세요 : ");
+		String id = sc.next();
+		// "검색할 아이디를 입력하세요 : " 출력 후 키보드로 아이디 입력 받음
 
+		for (int i = 0; i < MemberManager.ctn; i++) {
+			// 회원 정보 저장용 배열의 0번부터 idx값까지 입력받은 아이디와 객체 배열의 아이디를 비교를 반복
+			if (id.equals(m[i].getUserId())) {
+				printOne(m[i]);
+				// 일치하는 인덱스값 찾아서 해당하는 배열의 인덱스 객체 정보 출력
+				// 단,printOne()사용하여 출력
+			} else {
+				System.out.println("\"검색한 회원 정보가 존재하지 않습니다.\"");
+				return;
+				// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
+			}
+		}
 	}
 
+	
 	public void updateMemberMenu() {
 		// TODO Auto-generated method stub
 
@@ -100,4 +115,12 @@ public class MemberManager {
 
 	}
 
+	public void printOne(Member m) {
+		System.out.println("id : " + m.getUserId());
+		System.out.println("pw : " + m.getUserPwd());
+		System.out.println("이름 : " + m.getUserName());
+		System.out.println("나이 : " + m.getAge());
+		System.out.println("성별 : " + m.getGender());
+		System.out.println("이메일 : " + m.getEmail());
+	}
 }

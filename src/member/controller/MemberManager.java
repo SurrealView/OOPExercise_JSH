430d6745340de4a7164efc1ff8187e066ab201f6
Quartermoon <Quartermoon@DESKTOP-94OSCIF>
2020-03-24 12:04:25 +0900

    Correct wrong methods in MamberManager

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -43,26 +43,28 @@ public class MemberManager {
 	public void searchId() {
 		System.out.print("검색할 아이디를 입력하세요 : ");
 		String id = sc.next();
+		int exist = 0;
 		// "검색할 아이디를 입력하세요 : " 출력 후 키보드로 아이디 입력 받음
 
 		for (int i = 0; i < MemberManager.ctn; i++) {
 			// 회원 정보 저장용 배열의 0번부터 idx값까지 입력받은 아이디와 객체 배열의 아이디를 비교를 반복
 			if (id.equals(m[i].getUserId())) {
 				printOne(m[i]);
+				exist++;
 				// 일치하는 인덱스값 찾아서 해당하는 배열의 인덱스 객체 정보 출력
 				// 단,printOne()사용하여 출력
-			} else {
-				System.out.println("검색한 회원 정보가 존재하지 않습니다.");
-				return;
-				// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
 			}
 		}
+		if (exist == 0)
+			System.out.println("검색한 회원 정보가 존재하지 않습니다.");
+		// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
 	}
 
 	public void searchName() {
 
 		System.out.print("검색할 이름을 입력하세요 : ");
 		String name = sc.next();
+		int exist = 0;
 		// 아이디 찾기와 동일
 		// 이름을 입력하여 이름을 비교하여 검색된 회원 정보 출력
 
@@ -70,39 +72,41 @@ public class MemberManager {
 			// 회원 정보 저장용 배열의 0번부터 idx값까지 입력받은 아이디와 객체 배열의 아이디를 비교를 반복
 			if (name.equals(m[i].getUserName())) {
 				printOne(m[i]);
+				exist++;
 				// 일치하는 인덱스값 찾아서 해당하는 배열의 인덱스 객체 정보 출력
 				// 단,printOne()사용하여 출력
-			} else {
-				System.out.println("검색한 회원 정보가 존재하지 않습니다.");
-				return;
-				// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
 			}
 		}
+		if (exist == 0)
+			System.out.println("검색한 회원 정보가 존재하지 않습니다.");
+		// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
 	}
 
 	public void searchEmail() {
 
 		System.out.print("검색할 이메일을 입력하세요 : ");
 		String email = sc.next();
+		int exist = 0;
 		// 이메일로 검색하여 위의 메소드와 동일한 방식으로 작성
 
 		for (int i = 0; i < MemberManager.ctn; i++) {
 			// 회원 정보 저장용 배열의 0번부터 idx값까지 입력받은 아이디와 객체 배열의 아이디를 비교를 반복
 			if (email.equals(m[i].getEmail())) {
 				printOne(m[i]);
+				exist++;
 				// 일치하는 인덱스값 찾아서 해당하는 배열의 인덱스 객체 정보 출력
 				// 단,printOne()사용하여 출력
-			} else {
-				System.out.println("검색한 회원 정보가 존재하지 않습니다.");
-				return;
-				// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
 			}
+			// 일치하는 회원이 없는 경우 출력 후 메인으로 돌아감.
 		}
+		if (exist == 0)
+			System.out.println("검색한 회원 정보가 존재하지 않습니다.");
 	}
 
 	public void updatePwd() {
 		System.out.print("수정할 회원의 아이디를 입력하세요 : ");
 		String id = sc.next();
+		int exist = 0;
 		// "수정할 회원의 아이디를 입력하세요 : " 출력 후 키보드로 아이디 입력 받음
 
 		for (int i = 0; i < MemberManager.ctn; i++) {
@@ -115,13 +119,12 @@ public class MemberManager {
 				System.out.println("패스워드 수정이 완료되었습니다 ");
 				return;
 				// "패스워드 수정이 완료되었습니다 출력 후 메인메뉴로 돌아감.
-			} else {
-				System.out.println("수정할 회원이 존재하지 않습니다.");
-				return;
-				// 수정하려는 회원이 없는 경우 "수정할 회원이 존재하지 않습니다." 출력 후 메인 메뉴로 돌아감.
 			}
 
 		}
+		if (exist == 0)
+			System.out.println("수정할 회원이 존재하지 않습니다.");
+		// 수정하려는 회원이 없는 경우 "수정할 회원이 존재하지 않습니다." 출력 후 메인 메뉴로 돌아감.
 
 	}
 
@@ -129,7 +132,7 @@ public class MemberManager {
 		System.out.print("수정할 회원의 아이디를 입력하세요 : ");
 		String id = sc.next();
 		// 비밀번호 변경과 동일함 (수정할 정보는 변경할 이름을 입력받아 setter로 변경한다.)
-
+		int exist = 0;
 		for (int i = 0; i < MemberManager.ctn; i++) {
 			if (id.equals(m[i].getUserId())) {
 				// search방식과 동일하게 검색하여 해당 인덱스 찾은 후
@@ -139,20 +142,17 @@ public class MemberManager {
 				System.out.println("패스워드 수정이 완료되었습니다 ");
 				return;
 				// "패스워드 수정이 완료되었습니다 출력 후 메인메뉴로 돌아감.
-			} else {
-				System.out.println("수정할 회원이 존재하지 않습니다.");
-				return;
-				// 수정하려는 회원이 없는 경우 "수정할 회원이 존재하지 않습니다." 출력 후 메인 메뉴로 돌아감.
 			}
-
 		}
+		if(exist == 0)
+			System.out.println("수정할 회원이 존재하지 않습니다.");
 	}
 
 	public void updateEmail() {
 		System.out.print("수정할 회원의 아이디를 입력하세요 : ");
 		String id = sc.next();
 		// 비밀번호 변경과 동일함 (수정할 정보는 변경할 이메일 주소를 입력받아 setter로 변경한다.)
-
+		int exist = 0;
 		for (int i = 0; i < MemberManager.ctn; i++) {
 			if (id.equals(m[i].getUserId())) {
 				// search방식과 동일하게 검색하여 해당 인덱스 찾은 후
@@ -169,6 +169,8 @@ public class MemberManager {
 			}
 
 		}
+		if (exist == 0)
+			System.out.println("수정할 회원이 존재하지 않습니다.");
 	}
 
 	public void deleteOne() {
@@ -227,9 +229,9 @@ public class MemberManager {
 			System.out.println("나이 : " + m[i].getAge());
 			System.out.println("성별 : " + m[i].getGender());
 			System.out.println("이메일 : " + m[i].getEmail());
-			
+
 		}
-		//반복문을 이용하여 ctn까지의 모든 인덱스의 객체 정보를 getter를 통해 출력시키는 메소드
+		// 반복문을 이용하여 ctn까지의 모든 인덱스의 객체 정보를 getter를 통해 출력시키는 메소드
 
 	}
 
